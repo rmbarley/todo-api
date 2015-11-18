0417296cf44f276e83714826833031e089d09422
Ryan Barley <rmbarley@gmail.com>
2015-11-17 23:03:45 -0500

    Added PUSH to API

--- server.js
+++ server.js
@@ -33,14 +33,15 @@ app.get("/todos/:id", function (req, res) {
 
 // POST /todos
 app.post("/todos", function (req, res) {
-  var body = req.body;
-  // Validate and mung data
-  body = _.pick(body, "description", "completed");
-  body.description = body.description.trim();
-  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-    return res.status(400).send();
+  var body = _.pick(req.body, "description", "completed");
 
+  // Validate and mung data
+  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+  return res.status(400).send();
   }
+
+  body.description = body.description.trim();
+
   //add id field
   body.id = todoNextId;
   todoNextId++;
@@ -54,12 +55,43 @@ app.post("/todos", function (req, res) {
 app.delete("/todos/:id", function (req, res) {
   var todoId = parseInt(req.params.id, 10);
   var matchedTodo = _.findWhere(todos, {id: todoId});
+
   if (!matchedTodo) {
     res.status(404).json({"error": "no item found with that id."});
   } else {
     todos = _.without(todos, matchedTodo);
     res.send("Item was deleted successfully.");
   }
+});
+
+// PUT /todos/:id
+app.put("/todos/:id", function (req, res) {
+  var todoId = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {id: todoId});
+
+  var body = _.pick(req.body, "description", "completed");
+  var validAttributes = {};
+
+  if (!matchedTodo) {
+    return res.status(404).send();
+  }
+
+  if (body.hasOwnProperty("completed") && _.isBoolean(body.completed)) {
+    validAttributes.completed = body.completed;
+  } else if (body.hasOwnProperty("completed")) {
+    // Something bad happened
+    return res.status(400).send();
+  }
+
+  if (body.hasOwnProperty("description") && _.isString(body.description) && body.description.trim().length > 0) {
+    validAttributes.description = body.description;
+  } else if (body.hasOwnProperty("description")) {
+    return res.status(400).send();
+  }
+
+  // HERE
+  _.extend(matchedTodo, validAttributes);
+  res.json(matchedTodo);
 });
 
 app.listen(PORT, function () {
