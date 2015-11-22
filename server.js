74394541dad99ee0f46e29da47950f2987e3df68
Ryan Barley <rmbarley@gmail.com>
2015-11-21 22:41:04 -0500

    Added /GET by id

--- server.js
+++ server.js
@@ -43,15 +43,16 @@ app.get("/todos", function(req, res) {
 // GET /todos/:id
 app.get("/todos/:id", function(req, res) {
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo = _.findWhere(todos, {
-    id: todoId
-  });
 
-  if (matchedTodo) {
-    res.json(matchedTodo);
-  } else {
-    res.status(404).send();
-  }
+  db.todo.findById(todoId).then(function(todo) {
+    if (!!todo) {
+      res.json(todo.toJSON());
+    } else {
+      res.status(404).send();
+    }
+  }, function(e) {
+    res.status(500).send();
+  });
 });
 
 // POST /todos
@@ -64,21 +65,6 @@ app.post("/todos", function(req, res) {
     res.status(400).json(e);
 
   });
-
-  // // Validate and mung data
-  // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-  //   return res.status(400).send();
-  // }
-
-  // body.description = body.description.trim();
-
-  // //add id field
-  // body.id = todoNextId;
-  // todoNextId++;
-  // // push body into array
-  // todos.push(body);
-
-  // res.json(body);
 });
 
 // Delete /todos/:id
